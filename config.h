b57f6447fc231b87db2f8dff9b519b7b4d1536dc
maki10111 <makibyet101@gmail.com>
2020-10-13 13:37:12 +0800

    removed some items

--- config.h
+++ config.h
@@ -2,27 +2,11 @@
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-//	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-//	 {"",	"music",	0,	11},
-	{"",	"pacpackages",	0,	8},
-//	{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
-	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
-//	{"",	"torrent",	20,	7},
-	{"", 	"mailbox",	20,	0},
-	{"",	"memory",	5,	0},
-//	{"",	"cpu",		10,	18},
-	/* {"",	"moonphase",	18000,	17}, */
-//	{"",	"weather",	18000,	5},
-//	{"",	"disk",		60,	1},
-	{"",	"volume",	1,	10},
-	{"",	"pingbar",	5,	0},
-//	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	0},
-//	{"",	"internet",	1,	10},
-//	{"", "randombg",	0,     5},
+	{"",	"pacpackages",	0,	5},
+	{"", 	"mailbox",	20,	4},
+	{"",	"freemem",	5,	3},
+	{"",	"volume",	0,	2},
+	{"",	"clock",	60,	1},
 
 
 };
